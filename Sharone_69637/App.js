12d14b381d2cac3c4880efa0b1230d39e1d44b9e
sharonerosee <129519148+sharonerosee@users.noreply.github.com>
2024-10-09 10:48:17 +0700

    tutor

--- Sharone_69637/App.js
+++ Sharone_69637/App.js
@@ -1,40 +1,62 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native';
-import Counter from "./Counter";
-import Profile from './Profile';
+// import Counter from "./Counter";
+// import Profile from './Profile';
 import { useState } from "react";
 
-export default function App() {
-  const [count, setCount] = useState(0);
-  const [nama, setNama] = useState('');
-  const [showProfile, setShowProfile] = useState(false);
+const InputNama = ({nama, onChangeText}) => {
+  console.log(nama);
+  return (
+    <View>
+      <TextInput
+        placeholder='Masukkan nama disini'
+        style={{
+          borderColor: 'black',
+          borderWidth: 1,
+          padding: 10,
+          borderRadius: 8,
+        }}
+        onChangeText={onChangeText}
+        value={nama}
+        />
+    </View>
+  );
+};
 
-  const handleIncrement = () => {
-    setCount(count+1);
-  };
-  
-  const handleDecrement = () => {
-    setCount(count-1);
+const InputNIM = ({nim, onChangeText}) => {
+  console.log(nim);
+  return (
+    <View>
+      <TextInput
+        placeholder='Masukkan nim disini'
+        style={{
+          borderColor: 'black',
+          borderWidth: 1,
+          padding: 10,
+          borderRadius: 8,
+        }}
+        onChangeText={onChangeText}
+        value={nim}
+        />
+    </View>
+  );
+};
+
+export default function App(){
+  const [nama, setNama] = useState("");
+  const handleChangeMyName = (value) => {
+    setNama(value);
   };
 
-  const handlePassValue = () => {
-    setShowProfile(true);
+  const [nim, setNIM] = useState("");
+  const handleChangeMyNIM = (value) => {
+    setNIM(value);
   };
 
-  return(
+  return (
     <View style={styles.container}>
-      {showProfile && <Profile nama={nama} umur={count}/>}
-      <Counter
-       value={count}
-       handleDecrement={handleDecrement}
-       handleIncrement={handleIncrement}
-       handlePassValue={handlePassValue}
-      />
-      <TextInput 
-      style={styles.input}
-      placeholder="Input your name here"
-      value={nama}
-      onChangeText={setNama}
-      />
+      <Text>{nama} - {nim} </Text>
+      <InputNama nama={nama} onChangeText={handleChangeMyName}/>
+      <InputNIM nim={nim} onChangeText={handleChangeMyNIM}/>
     </View>
   );
 }
@@ -46,15 +68,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flex: 1,
     padding: 20,
-  },
-  input:{
-    borderWidth: 1,
-    borderColor: '#000',
-    padding: 10,
-    width: '80%',
-    marginVertical: 10,
-    borderRadius: 5,
-    textAlign: 'center',
-  },
-
+  }
 });
